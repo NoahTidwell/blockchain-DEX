3e76294baadeb81beaff1599df08b41dc1a55f23
Noah <ntidwell34@yahoo.com>
2022-07-25 19:22:51 -0500

    create withdraw token functionality and check balances

--- test/Exchange.js
+++ test/Exchange.js
@@ -80,4 +80,65 @@ describe('Exchange', () => {
 
   })
 
+  describe('Withdrawing Tokens', () => {
+    let tx, result
+    let amount = tokens(10)
+
+    describe('Success', () => {
+      beforeEach(async () => {
+
+      tx = await token1.connect(user1).approve(exchange.address, amount)
+      result = await tx.wait()
+
+      tx = await exchange.connect(user1).depositToken(token1.address, amount)
+      result = await tx.wait()
+
+      tx = await exchange.connect(user1).withdrawToken(token1.address, amount)
+      result = await tx.wait()
+      })
+
+        it('withdraws the token funds', async () => {
+          expect(await token1.balanceOf(exchange.address)).to.equal(0)
+          expect(await exchange.tokens(token1.address, user1.address)).to.equal(0)
+          expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(0)
+        })
+
+        it('emits a Withdraw event', async () => {
+        const event = result.events[1] // 2 events are emitted
+        expect(event.event).to.equal('Withdraw')
+
+        const args = event.args
+        expect(args.token).to.equal(token1.address)
+        expect(args.user).to.equal(user1.address)
+        expect(args.amount).to.equal(amount)
+        expect(args.balance).to.equal(0)
+      })
+
+    })
+
+    describe('Failure', () => {
+      it('fails for insufficient balance', async () => {
+        await expect(exchange.connect(user1).withdrawToken(token1.address, amount)).to.be.reverted
+      })
+    })
+
+  })
+
+  describe('Checking Balances', () => {
+    let tx, result
+    let amount = tokens(1)
+
+    beforeEach(async () => {
+      tx = await token1.connect(user1).approve(exchange.address, amount)
+      result = await tx.wait()
+
+      tx = await exchange.connect(user1).depositToken(token1.address, amount)
+      result = await tx.wait()
+      })
+
+        it('returns user balance', async () => {
+          expect(await exchange.balanceOf(token1.address, user1.address)).to.equal(amount)
+        })
+  })
+
 })
